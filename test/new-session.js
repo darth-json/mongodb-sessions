79b6d5f83fa820e3e292d59c4694b88d41f9fdac
Jason Rall <jason.rall@charter.com>
2016-02-04 13:02:45 -0700

    Working cookie test

--- test/new-session.js
+++ test/new-session.js
@@ -1,22 +1,18 @@
 var request = require('supertest')(require('../app'))
 var assert = require('chai').assert
+var expect = require('chai').expect
 
 describe('the home page', function () {
   it('should not set a cookie before login', function (done) {
-    /**
-     * - edit `test/new-session.js` to test that the site does NOT set a cookie on the home page
-     * - once you write the test, this should already pass
-     * - write a test that the home page shows the login form
-     * - use the `cheerio` module to parse the response HTML and check for the `div.login` selector to be present
-     * - make the test pass
-     */
-    request.get('/').expect(200).end((err, res) =>{
-      console.log(JSON.stringify(res));
+    request.get('/').expect(200).expect().end((err, res) =>{
+      expect(res.headers).to.not.have.property('set-cookie');
       done();
     });
   })
 
   it('should show login form', function (done) {
-
+    assert.equals(0,1);
+    done();
   })
 })
+ 
--- test/register.js
+++ test/register.js
@@ -5,22 +5,28 @@ var validNewUser = require('./valid-new-user')
 
 describe('new user registration', function () {
   it('should require email', function (done) {
-
+    assert.equals(0,1);
+    done();
   })
 
   it('should require password', function (done) {
-
+    assert.equals(0,1);
+    done();
   })
 
   it('should require password at least 4 characters', function (done) {
-
+    assert.equals(0,1);
+    
+    done();
   })
 
   it('should work with 201', function (done) {
-
+    assert.equals(0,1);
+    done();
   })
 
   it('should detect duplicate registration attempts', function (done) {
-
+    assert.equals(0,1);
+    done();
   })
 })
