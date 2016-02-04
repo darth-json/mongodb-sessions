c824eeb17da7efb16c982ede51e8dc2b7d7c1088
Jason Rall <jason.rall@charter.com>
2016-02-04 13:23:15 -0700

    Login form test

--- test/new-session.js
+++ test/new-session.js
@@ -1,18 +1,24 @@
 var request = require('supertest')(require('../app'))
 var assert = require('chai').assert
 var expect = require('chai').expect
+var cheerio = require('cheerio');
 
 describe('the home page', function () {
   it('should not set a cookie before login', function (done) {
-    request.get('/').expect(200).expect().end((err, res) =>{
+    request.get('/').expect(200).end((err, res) =>{
       expect(res.headers).to.not.have.property('set-cookie');
       done();
     });
   })
 
   it('should show login form', function (done) {
-    assert.equals(0,1);
-    done();
+    request.get('/').expect(200).end((err, res) =>{
+      var $ = cheerio.load(res.text);
+      var html = $('div.login').html()
+      console.log(html);
+      expect(html).not.to.be.null;
+      done();
+    });
   })
 })
  
